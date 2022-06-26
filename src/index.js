0ea407d05ca46cac066e5166041aa1908e127af6
natalka-kar <nataryades@gmail.com>
2022-06-26 12:24:09 +0300

    init: start and finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let o = Math.abs(n);
+  let a = o.toString(10);
+  let b = a.split('');
+  let c = b.reverse();
+  let d = c.join('');
+  let f = Number(d);
+  return f;
 }
